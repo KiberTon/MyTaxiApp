366e89073f4c226acf337b32b72e1e8bedb532b1
Anton Kibekin <kibekin1984anton@gmail.com>
2020-04-13 16:48:04 +0300

    Splash screen java код

--- app/src/main/java/com/kibekin/mytaxiapp/PassengerSignInActivity.java
+++ app/src/main/java/com/kibekin/mytaxiapp/PassengerSignInActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignInActivity extends AppCompatActivity {
+public class PassengerSignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_passenger_sign_in);
     }
 }
