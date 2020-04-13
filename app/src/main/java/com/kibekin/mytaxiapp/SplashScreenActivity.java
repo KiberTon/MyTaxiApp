c9fc111f3febf20c70e44fb6015758147313f56e
Anton Kibekin <kibekin1984anton@gmail.com>
2020-04-13 12:52:57 +0300

    Create SignInActivity and start thread

--- app/src/main/java/com/kibekin/mytaxiapp/SplashScreenActivity.java
+++ app/src/main/java/com/kibekin/mytaxiapp/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.kibekin.mytaxiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -11,6 +12,24 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
